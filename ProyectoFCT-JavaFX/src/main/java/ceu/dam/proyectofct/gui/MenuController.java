57256d6a9d684f622cdae2d426ad061a94faf199
juanan04 <ja.aragon23@gmail.com>
2025-02-13 17:58:28 +0100

    Added new .fxml (userHomepage.fxml, menu.fxml) and their respective controllers (UserHomepageController.java, MenuController.java). Updated AppController.java with new method (exitApplication)

---
+++ ProyectoFCT-JavaFX/src/main/java/ceu/dam/proyectofct/gui/MenuController.java
@@ -0,0 +1,42 @@
+package ceu.dam.proyectofct.gui;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
+public class MenuController extends AppController{
+
+	@FXML
+	private BorderPane panel;
+	
+	private static BorderPane sharedPanel;
+	
+	public void initialize() {
+		sharedPanel = panel;
+	}
+	
+	public static void loadPage(String fxmlPath) {
+		sharedPanel.setCenter(loadScene(fxmlPath));
+	}
+	
+    @FXML
+    void changePass(ActionEvent event) {
+    	panel.setCenter(loadScene(FXML_CHNGPASS));
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+    	panel.setCenter(loadScene(FXML_LOGIN));
+    }
+
+    @FXML
+    void seeRecords(ActionEvent event) {
+    	panel.setCenter(loadScene(FXML_RECORDS));
+    }
+    
+    @FXML
+    void homePage(ActionEvent event) {
+    	panel.setCenter(loadScene(FXML_USERHOME));
+    }
+
+}
